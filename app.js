44deb71e710586aab67b1826eead2cf1d36bab57
Amar Chauhan <89099211+ChauhanAmar@users.noreply.github.com>
2022-04-12 23:30:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,3 +25,33 @@ app.get("/Contact", (req,res)=>{
 app.listen(8000, ()=>{
     console.log("listening server 8000");
 });
+
+
+
+//Dynamic website
+const express=require("express");
+const path=require("path");
+const app=express();
+const port=8000;
+
+//built in middleware
+//console.log(path.join(__dirname,"../public"));
+const staticPath=path.join(__dirname,"../public");
+app.use(express.static(staticPath));
+
+app.get("/", (req,res)=>{
+    res.send("Hello World");
+
+});
+app.get("/About", (req,res)=>{
+    res.send("Hello World About");
+
+});
+app.get("/Contact", (req,res)=>{
+    res.send("Hello World Contact");
+
+});
+
+app.listen(8000, ()=>{
+    console.log(`listening server ${port}`);
+});
